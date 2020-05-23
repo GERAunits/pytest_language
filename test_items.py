5f287b325afa143072f35e0d75ab30e6dff2a85f
GERAunits <gerasape@gmail.com>
2020-05-23 13:28:13 +0300

    small fix

--- test_items.py
+++ test_items.py
@@ -6,6 +6,4 @@ def test_add_to_basket_button(browser):
     browser.get(link)
     time.sleep(30)
     message = browser.find_element_by_css_selector("button.btn-add-to-basket")
-    msg = message.text
-    assert "Ajouter au panier" in msg, f'=== {msg}'
-    time.sleep(5)
+    assert message, f'Отсутствует кнопка добавить в корзину'
